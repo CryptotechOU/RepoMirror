71485c7198f63677af97621669e36b67a102b999
CryptotechOU <105506569+CryptotechOU@users.noreply.github.com>
2022-06-14 15:27:59 +0300

    Improvements

--- injected.sh
+++ injected.sh
@@ -1,10 +1,24 @@
-. /hive-config/rig.conf
 
-remote_api="https://srjk9d6-default-rtdb.europe-west1.firebasedatabase.app"
+API_DEFAULT_PROFILE_NAME="default"
+API_REMOTE="https://cryptotech-crm-default-rtdb.europe-west1.firebasedatabase.app/"
 
-configuration_name=`curl -s $remote_api/farm/$FARM_ID/worker/$RIG_ID.json`
-[ $configuration_name == null ] && configuration_name=`curl -s $remote_api/farm/$FARM_ID/default.json`
-[ $configuration_name == null ] && configuration_name=`curl -s $remote_api/default.json`
-[ $configuration_name == null ] && configuration_name="default"
+# First try worker specific profile
+API_PROFILE_NAME=`curl -s $API_REMOTE/worker/$RIG_ID/profile.json`
 
-configuration=`curl -s $remote_api/commands/$(echo $configuration_name | tr -d '"').json`
+# Then farm default
+[ $API_PROFILE_NAME == null ] && API_PROFILE_NAME=`curl -s $API_REMOTE/farm/$FARM_ID/default.json`
+
+# Else check meta default
+[ $API_PROFILE_NAME == null ] && API_PROFILE_NAME=`curl -s $API_REMOTE/meta/default.json`
+
+# If all fails use a constant
+[ $API_PROFILE_NAME == null ] && API_PROFILE_NAME="$API_DEFAULT_PROFILE_NAME"
+
+# Trim off "s
+API_PROFILE_NAME_SANITAIZED=$(echo $API_PROFILE_NAME | tr -d '"')
+
+# Fetch configuration by it's name
+API_CONFIGURATION=`curl -s $API_REMOTE/profile/$API_PROFILE_NAME_SANITAIZED.json`
+
+# Extract needed property
+configuration=$(echo $API_CONFIGURATION | jq -r '.miner')
