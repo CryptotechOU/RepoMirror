4b798fd617e3beaed70b008e66379315388c77d3
CryptotechOU <105506569+CryptotechOU@users.noreply.github.com>
2022-07-06 16:12:19 +0300

    Change data format

--- sendConsumptions.js
+++ sendConsumptions.js
@@ -9,7 +9,7 @@ async function main() {
 	const token = JSON.parse(await fs.readFile('./token.json', 'utf-8'))
 	const api = new HiveAPI(token)
 
-	const date = (new Date()).toISOString().slice(0, 16).replaceAll('-', '/').replace('T', '/').replace(':', '/')
+	const date = new Date()
 
 	console.log('Sending with date:', date)
 
@@ -20,9 +20,14 @@ async function main() {
 
 		return Promise.all(workers.map(worker => {
 			const url = 'https://cryptotech-crm-default-rtdb.europe-west1.firebasedatabase.app/consumption/' +
-				`${farm.id}/${worker.id}/${date}.json`
+				`${farm.id}/${worker.id}.json`
 
-			return fetch(url, { method: 'PUT', body: worker.data.stats.power_draw || 0 })
+			const body = JSON.stringify({
+				consumption: worker.data.stats.power_draw || 0,
+				timestamp: date.toISOString()
+			})
+
+			return fetch(url, { method: 'PUT', body })
 				.then(() => console.log('[SENT]', farm.name, '-', worker.name, '-', consumption, 'W'))
 				.catch(e => console.error('[ERROR]', farm.name, '-', worker.name))
 		}))
